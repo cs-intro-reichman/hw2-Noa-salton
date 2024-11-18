84d14423b28be29fcb3230616aa3a299bd790afb
Noa Salton <noasalton@Noas-MacBook-Pro.local>
2024-11-18 21:56:45 +0200

    CHEERS

--- Cheers.java
+++ Cheers.java
@@ -6,13 +6,13 @@ public class Cheers {
             String anLetters = "AEFHILMNORSX";
             for (int i=0; i<nameToCheer.length(); i++){
                 String pre = "a";
-                if(anLetters.indexOf(nameToCheer.charAt(i)) != -1){
+                if(anLetters.indexOf(Character.toUpperCase(nameToCheer.charAt(i))) != -1){
                         pre = "an";
                 }
-                System.out.println("Give me " + pre + " " + nameToCheer.charAt(i) + ": " + nameToCheer.charAt(i) + "!");
+                System.out.println("Give me " + pre + " " + Character.toUpperCase(nameToCheer.charAt(i)) + ": " + Character.toUpperCase(nameToCheer.charAt(i)) + "!");
         }
         System.out.println("What does that spell?");
-        for (int j=0; j<timesToCheer; j++){System.out.println(nameToCheer + "!!!");}
+        for (int j=0; j<timesToCheer; j++){System.out.println(nameToCheer.toUpperCase() + "!!!");}
 
 
         }
